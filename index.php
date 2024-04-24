945b895a620a6075602091c0f7d01547126903aa
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-24 18:15:12 +0200

    create file htaccess to set base app entry point

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 require 'functions.php';
 
 $heading = 'Home';
+$uri = $_SERVER['REQUEST_URI'];
 
 
-require 'views/index.view.php';
+if ($uri === '/laracast/php-for-beginner/') {
+    require 'views/index.view.php';
+} else if ($uri === '/laracast/php-for-beginner/about') {
+    require 'views/about.view.php';
+} else if ($uri === '/laracast/php-for-beginner/contact') {
+    require 'views/contact.view.php';
+}
