a407116ed24a56c6f1cc0ededbd61c03f0cbda8f
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-25 11:24:05 +0200

    connect to db and print posts to page

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to our MySql database
+$dsn = "mysql:host=localhost;port=8889;dbname=php_tailwind_app;user=root;password=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC); // format result into assoc array to remove indexes
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
