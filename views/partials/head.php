e41893681b9df2f0fff685368edd3fe9e78266b6
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-25 21:10:42 +0200

    add form for creating new note

--- views/partials/head.php
+++ views/partials/head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2"></script>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
