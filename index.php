a8a2b2846237bd773dd3cb167e69f5038cdfc9c2
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-25 10:41:19 +0200

    add function responsible to direct the route to its specific controller

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'functions.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
@@ -11,16 +12,23 @@ $routes = [
     '/laracast/php-for-beginner/contact' => 'controllers/contact.php',
 ];
 
+
+function routeToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+
+routeToController($uri, $routes);
+
+
 function abort($code = 404)
 {
     http_response_code($code);
     require "views/$code.php";
 
     die();
-}
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    abort();
 }
