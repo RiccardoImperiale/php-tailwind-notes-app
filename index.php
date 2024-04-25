47f57b79cd9a0515bbeaea56a0bae2f539c3c16f
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-25 14:09:54 +0200

    fix htaccess file to allow query string parameters

--- index.php
+++ index.php
@@ -7,9 +7,15 @@ require 'Database.php';
 $config = require 'config.php';
 
 $db = new Database($config['database']);
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC); // format result into assoc array to remove indexes
 
 
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
+$id = $_GET['id'];
+echo $id;
+
+// $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC); // format result into assoc array to remove indexes
+$post = $db->query("SELECT * FROM posts WHERE id = 1")->fetch();
+
+var_dump($post);
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
