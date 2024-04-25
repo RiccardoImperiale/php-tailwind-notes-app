f30efdabdf36cdb2236a2276798a3196c718807b
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-25 17:46:38 +0200

    render notes and add single note page

--- index.php
+++ index.php
@@ -4,12 +4,10 @@ require 'functions.php';
 require 'Database.php';
 require 'router.php';
 
-$config = require 'config.php';
-
-$db = new Database($config['database']);
 
 
-$id = $_GET['id'];
+
+
 
 // question mark way
 // $query = "SELECT * FROM posts WHERE id = ?";
@@ -20,7 +18,7 @@ $id = $_GET['id'];
 // $post = $db->query($query, [':id' => $id])->fetch();
 // $posts = $db->query($query)->fetchAll(PDO::FETCH_ASSOC); // format result into assoc array to remove indexes
 
-var_dump($post);
+
 // foreach ($posts as $post) {
 //     echo "<li>" . $post['title'] . "</li>";
 // }
--- router.php
+++ router.php
@@ -8,6 +8,7 @@ $routes = [
     "$base_url/" => 'controllers/index.php',
     "$base_url/about" => 'controllers/about.php',
     "$base_url/notes" => 'controllers/notes.php',
+    "$base_url/note" => 'controllers/note.php',
     "$base_url/contact" => 'controllers/contact.php',
 ];
 
