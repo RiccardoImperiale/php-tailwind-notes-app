edd599d9f12c8b8fd1bbf840ced1fd67125abf64
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-25 11:32:01 +0200

    add Database class

--- index.php
+++ index.php
@@ -3,14 +3,25 @@
 require 'functions.php';
 // require 'router.php';
 
-// connect to our MySql database
-$dsn = "mysql:host=localhost;port=8889;dbname=php_tailwind_app;user=root;password=root;charset=utf8mb4";
+// connect to database and execute a query
+class Database
+{
+    public function query($query): array
+    {
+        $dsn = "mysql:host=localhost;port=8889;dbname=php_tailwind_app;user=root;password=root;charset=utf8mb4";
 
-$pdo = new PDO($dsn);
+        $pdo = new PDO($dsn);
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC); // format result into assoc array to remove indexes
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC); // format result into assoc array to remove indexes
+    }
+}
+
+$db = new Database();
+
+$posts = $db->query("SELECT * FROM posts WHERE id > 1");
+
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
