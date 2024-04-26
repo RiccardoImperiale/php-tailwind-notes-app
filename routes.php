178aef2086fe43cf5c578933b23bc6b51492a849
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-26 11:10:22 +0200

    add resourceful naming conventions

--- routes.php
+++ routes.php
@@ -5,8 +5,8 @@ $base_url = '/laracast/php-for-beginner';
 return [
     "$base_url/" => 'controllers/index.php',
     "$base_url/about" => 'controllers/about.php',
-    "$base_url/notes" => 'controllers/notes.php',
-    "$base_url/note" => 'controllers/note.php',
-    "$base_url/notes/create" => 'controllers/note-create.php',
+    "$base_url/notes" => 'controllers/notes/index.php',
+    "$base_url/note" => 'controllers/notes/show.php',
+    "$base_url/notes/create" => 'controllers/notes/create.php',
     "$base_url/contact" => 'controllers/contact.php',
 ];
