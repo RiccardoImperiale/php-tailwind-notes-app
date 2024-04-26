0a6f802c0c8578944bdcf8bfe7681058005901a1
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-26 14:59:17 +0200

    extract the destroy action to its own file

--- routes.php
+++ routes.php
@@ -4,7 +4,9 @@ const BASE_URL = '/notes-app/public';
 
 $router->get(BASE_URL . '/', 'controllers/index.php');
 $router->get(BASE_URL . '/about', 'controllers/about.php');
+$router->get(BASE_URL . '/contact', 'controllers/contact.php');
+
 $router->get(BASE_URL . '/notes', 'controllers/notes/index.php');
 $router->get(BASE_URL . '/note', 'controllers/notes/show.php');
+$router->delete(BASE_URL . '/note', 'controllers/notes/destroy.php');
 $router->get(BASE_URL . '/notes/create', 'controllers/notes/create.php');
-$router->get(BASE_URL . '/contact', 'controllers/contact.php');
