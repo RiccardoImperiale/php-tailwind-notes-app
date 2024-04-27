4d1ff8ef3cc3eff1141650d26719b2b0b14fb889
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-27 15:04:17 +0200

    add http folder and move controllers into it

--- routes.php
+++ routes.php
@@ -3,24 +3,24 @@
 $config = require 'config.php';
 define('BASE_URL', $config['app']['BASE_URL']);
 
-$router->get(BASE_URL . '/', 'controllers/index.php');
-$router->get(BASE_URL . '/about', 'controllers/about.php');
-$router->get(BASE_URL . '/contact', 'controllers/contact.php');
+$router->get(BASE_URL . '/', 'index.php');
+$router->get(BASE_URL . '/about', 'about.php');
+$router->get(BASE_URL . '/contact', 'contact.php');
 
-$router->get(BASE_URL . '/notes', 'controllers/notes/index.php')->only('auth');
-$router->get(BASE_URL . '/note', 'controllers/notes/show.php');
-$router->delete(BASE_URL . '/note', 'controllers/notes/destroy.php');
+$router->get(BASE_URL . '/notes', 'notes/index.php')->only('auth');
+$router->get(BASE_URL . '/note', 'notes/show.php');
+$router->delete(BASE_URL . '/note', 'notes/destroy.php');
 
-$router->get(BASE_URL . '/note/edit', 'controllers/notes/edit.php');
-$router->patch(BASE_URL . '/note', 'controllers/notes/update.php');
+$router->get(BASE_URL . '/note/edit', 'notes/edit.php');
+$router->patch(BASE_URL . '/note', 'notes/update.php');
 
-$router->get(BASE_URL . '/notes/create', 'controllers/notes/create.php');
-$router->post(BASE_URL . '/notes', 'controllers/notes/store.php');
+$router->get(BASE_URL . '/notes/create', 'notes/create.php');
+$router->post(BASE_URL . '/notes', 'notes/store.php');
 
-$router->get(BASE_URL . '/register', 'controllers/registration/create.php')->only('guest');
-$router->post(BASE_URL . '/register', 'controllers/registration/store.php');
+$router->get(BASE_URL . '/register', 'registration/create.php')->only('guest');
+$router->post(BASE_URL . '/register', 'registration/store.php');
 
-$router->get(BASE_URL . '/login', 'controllers/session/create.php')->only('guest');
-$router->post(BASE_URL . '/login', 'controllers/session/store.php')->only('guest');
+$router->get(BASE_URL . '/login', 'session/create.php')->only('guest');
+$router->post(BASE_URL . '/login', 'session/store.php')->only('guest');
 
-$router->delete(BASE_URL . '/session', 'controllers/session/destroy.php')->only('auth');
+$router->delete(BASE_URL . '/session', 'session/destroy.php')->only('auth');
