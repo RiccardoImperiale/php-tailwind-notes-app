68c80865e6faedca3a69c434e1e78b245ea96c9d
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-27 09:51:58 +0200

    add register form and store controller that saves the new user

--- routes.php
+++ routes.php
@@ -16,3 +16,6 @@ $router->patch(BASE_URL . '/note', 'controllers/notes/update.php');
 
 $router->get(BASE_URL . '/notes/create', 'controllers/notes/create.php');
 $router->post(BASE_URL . '/notes', 'controllers/notes/store.php');
+
+$router->get(BASE_URL . '/register', 'controllers/registration/create.php');
+$router->post(BASE_URL . '/register', 'controllers/registration/store.php');
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -13,11 +13,11 @@
             <a href="./note/edit?id=<?= $note['id'] ?>" class="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit</a>
         </footer>
 
-        <!-- <form action="" method="post" class="mt-6">
+        <form action="" method="post" class="mt-6">
             <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" name="id" value="<?= $note['id'] ?>">
             <button type="submit" class="text-sm text-red-500">Delete</button>
-        </form> -->
+        </form>
 
     </div>
 </main>
