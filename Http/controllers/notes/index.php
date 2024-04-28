a8373d5cd0eda2b693260728b65f62bbba040f47
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-28 11:56:00 +0200

    show notes from the logged in user

--- Http/controllers/notes/index.php
+++ Http/controllers/notes/index.php
@@ -6,7 +6,15 @@ use Core\Database;
 
 $db = App::resolve(Database::class);
 
-$notes = $db->query('SELECT * FROM notes WHERE user_id = 1')->get();
+$currentUser = $_SESSION['user']['email'];
+
+$currentId = $db->query('SELECT id FROM users WHERE email = :currentUser', [
+    ':currentUser' => $currentUser
+])->find();
+
+$notes = $db->query('SELECT * FROM notes WHERE user_id = :currentId', [
+    ':currentId' => $currentId['id']
+])->get();
 
 view('notes/index.view.php', [
     'heading' => 'My Notes',
